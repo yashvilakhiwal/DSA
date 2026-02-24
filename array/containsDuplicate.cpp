8c0415a98c679fb67958d2cb24aa84fb97400f2a
yashvilakhiwal <yashvilakhiwal@gmail.com>
2026-02-25 00:43:19 +0530

    added

---
+++ array/containsDuplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i-1]==nums[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+};
