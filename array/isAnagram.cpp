82e4e4f3443cfcff415f2bf88c036ffd1d08678d
yashvilakhiwal <yashvilakhiwal@gmail.com>
2026-05-04 15:57:51 +0530

    added

---
+++ array/isAnagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        int count[26]={0};
+        for(int i=0;i<s.length();i++)
+        {
+            int idx=s[i]-'a';
+            count[idx]++;
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            int idx=t[i]-'a';
+            if(count[idx]==0)
+            {
+                return false;
+            }
+            count[idx]--;
+        }
+        return true;
+         
+    }
+};
