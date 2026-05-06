3e98fa295f387b2f58e8e44f3a3e034f8f241ab6
yashvilakhiwal <yashvilakhiwal@gmail.com>
2026-05-06 22:31:48 +0530

    added

---
+++ array/xorBeauty.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int xorBeauty(vector<int>& nums) {
+        int a=0,b=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            a=a|nums[i];
+            b=b^nums[i];
+        }
+        return (a&b);
+    }
+};
