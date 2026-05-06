0211b52edcf287718c6f7faba87fa4ad8aee74ff
yashvilakhiwal <yashvilakhiwal@gmail.com>
2026-05-06 14:24:54 +0530

    added

---
+++ array/setMismatch.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> count(n+1,0);
+        int duplicate=-1,missing=-1;
+        for(int num:nums)
+        {
+            count[num]++;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(count[i]==2) duplicate=i;
+            else if(count[i]==0) missing=i;
+        }
+        return {duplicate, missing};
+    }
+};
